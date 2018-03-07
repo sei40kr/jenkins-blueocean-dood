7852a440fafe7ab2bfeb60052734cb0d28a7043d
Seong Yong-ju <sei40kr@gmail.com>
2018-03-08 01:56:56 +0900

    Update run-in-docker.sh
    Add volumes to share with the host.

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -4,5 +4,7 @@ docker run \
     --name jenkins-blueocean-dind \
     -p 8080:8080 \
     --rm \
+    -v '/dev/urandom:/dev/random' \
+    -v '/var/run/docker.sock:/var/run/docker.sock' \
     -v "${PWD}/var/jenkins_home:/var/jenkins_home" \
     sei40kr/jenkins-blueocean-dind
